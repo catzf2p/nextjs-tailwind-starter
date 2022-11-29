92a969eba253d587179bd7e46c9339fff38a218a
catzf2p <catzf2p@gmail.com>
2022-11-29 16:08:57 +0700

    feat: support responsive ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,13 @@ function Index() {
                     className='mb-6 w-40 animate-[spin_5s_linear_infinite]'
                     src='react-logo.png'
                 ></img>
-                <h1 className='bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-4xl font-extrabold text-transparent'>
-                    Next.js + TypeScript + Tailwind CSS
-                </h1>
+                <div className='flex flex-col bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-center text-4xl font-extrabold text-transparent md:flex-row'>
+                    <span>Next.js</span>
+                    <span className='px-2'>+</span>
+                    <span>TypeScript</span>
+                    <span className='px-2'>+</span>
+                    <span>Tailwind CSS</span>
+                </div>
             </div>
         </>
     );
